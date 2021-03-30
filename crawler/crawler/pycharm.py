f35a550982c37a85e7382c1939770969601f05d2
mauriMedina <maurimedina89@gmail.com>
2021-03-30 01:47:36 -0400

    Created spider for Tupi

--- crawler/crawler/pycharm.py
+++ crawler/crawler/pycharm.py
@@ -1,6 +1,7 @@
 from scrapy import cmdline
-# cmdline.execute("scrapy crawl superseis_spider".split())
+cmdline.execute("scrapy crawl superseis_spider".split())
 # cmdline.execute("scrapy crawl casarica_spider".split())
 # cmdline.execute("scrapy crawl nissei_spider".split())
 # cmdline.execute("scrapy crawl salemma_spider".split())
 # cmdline.execute("scrapy crawl stock_spider".split())
+# cmdline.execute("scrapy crawl tupi_spider".split())
---
+++ crawler/crawler/spiders/tupi_spider.py
@@ -0,0 +1,68 @@
+import re
+
+import scrapy
+
+from ..items import ProductItem
+
+
+class TupiSpider(scrapy.Spider):
+    name = 'tupi_spider'
+    allowed_domains = ['tupi.com.py']
+    start_urls = ['https://www.tupi.com.py/']
+
+    custom_settings = {
+        'SHOP_NAME': 'tupi',
+        'SHOP_URL': 'https://www.tupi.com.py/',
+    }
+
+    category_url = 'https://www.tupi.com.py/rubros_paginacion'
+
+    def parse(self, response):
+        categories = response.css('.titulo_rubro a::attr(href)').getall()
+        for category in categories:
+            category_url = self.get_category_next_page_url(category)
+            self.logger.info("Going to category page --> {}".format(category_url))
+
+            yield scrapy.Request(url=category_url, callback=self.parse_category_page)
+
+    def parse_category_page(self, response):
+        products = response.css('.product-inner')
+        if products:
+            for product in products:
+                item = ProductItem()
+
+                name = product.css('.nombre_producto_ug a::text').get()
+                if name:
+                    item['name'] = name.strip()
+
+                # TODO get brand from product page
+
+                # This is the web price
+                price_content = product.css('.amount::text').getall()
+                if price_content:
+                    price = ''.join(price_content)
+                    price = price.replace('Gs', '')
+                    price = price.replace('.', '')
+                    price = price.strip()
+                    item['price'] = price
+
+                item['image_url'] = product.css('.wp-post-image::attr(src)').get()
+                item['url'] = product.css('.nombre_producto_ug a::attr(href)').get()
+
+                yield item
+
+            next_page_url = self.get_category_next_page_url(response.url)
+            self.logger.info("Going to next page --> {}".format(next_page_url))
+
+            yield scrapy.Request(url=next_page_url, callback=self.parse_category_page)
+        else:
+            self.logger.info('No Products found, reached last page of search')
+
+    def get_category_next_page_url(self, current_category_url):
+        found = re.findall(r"/(\d+)", current_category_url)
+
+        category = found[0]
+        current_page = int(found[1]) if len(found) > 1 else 0
+
+        # url example -> 'https://www.tupi.com.py/rubros_paginacion/9/1/'
+        return '/'.join([self.category_url, category, str(current_page + 1), ''])
